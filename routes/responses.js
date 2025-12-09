86804480b7642d00f1beb3a2d58b1f5307b4153e
Herbertia <117363174+CaracaraEXE@users.noreply.github.com>
2025-12-09 19:34:11 -0300

    AAAAAAAAAAAAAA

--- routes/responses.js
+++ routes/responses.js
@@ -12,6 +12,16 @@ const addResponse = async(req,res) => {
         return res.status(501).send({ message: "Hubo un error", error });
     }
 }
+
+router.get("/", async (_req, res) => {
+  try {
+    const responses = await Response.find().select("_id user choices timestamps");
+    return res.status(200).send({ message: "Todas las categorías", responses });
+  } catch (error) {
+    return res.status(500).send({ message: "Hubo un error", error });
+  }
+});
+
 router.post("/",addResponse);
 
 export default router;
